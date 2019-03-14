2737cb8dd8edbc6fcbc01f046e3e0e57fded7c8b
Stanislav Popov <popstas@gmail.com>
2019-03-14 23:00:58 +0500

    fix: variable substitution on zsh 5.7
    #6

--- command-time.plugin.zsh
+++ command-time.plugin.zsh
@@ -21,7 +21,7 @@ _command_time_precmd() {
 zsh_command_time() {
   if [ -n "$ZSH_COMMAND_TIME" ]; then
     timer_show=$(printf '%dh:%02dm:%02ds\n' $(($ZSH_COMMAND_TIME/3600)) $(($ZSH_COMMAND_TIME%3600/60)) $(($ZSH_COMMAND_TIME%60)))
-    print -P '%F{$ZSH_COMMAND_TIME_COLOR}`printf "${ZSH_COMMAND_TIME_MSG}\n" "$timer_show"`%f'
+    print -P "%F{$ZSH_COMMAND_TIME_COLOR}$(printf "${ZSH_COMMAND_TIME_MSG}\n" "$timer_show")%f"
   fi
 }
 
